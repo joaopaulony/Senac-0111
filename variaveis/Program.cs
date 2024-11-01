feea5800388672f0b04b809de4ec2759488dfec3
João Paulo <joaopaulofam@gmail.com>
2024-11-01 16:29:20 -0400

    Tipos de dados Nao Primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -19,3 +19,47 @@ char inicial = "J";
 // Tipos logicos 
 bool = estaLogado = true;
 bool = ativo = false;
+
+// Tipos de dados Nao Primitivos
+string saudacao = "Ola Mundo!";
+
+// Arrays
+int [] numeros = {1,2,3,4,5}; 
+string[] nomes = {"Joao","Maria","Sirlene"};
+
+// Classes 
+public class Pessoa 
+{
+    public string Nome;
+    public int idade;
+}
+
+// Structs
+public class ponto
+
+{
+    public int x;
+    public int y;
+}
+
+// Enums
+
+public enum diaDaSemana
+{
+    Segunda, Terca, Quarta, Quinta, Sexta, Sabado, Domingo
+}
+
+// Interfaces
+
+public interface IVeiculo
+{
+    void Mover();
+}
+
+// Delegates
+
+public void delegates Processar (int numero);
+
+// Nullabe types
+int? idade = null;
+
