c1e25caf06dc9ddf832c35af2e683c766dfe1b5f
João Paulo <joaopaulofam@gmail.com>
2024-11-01 16:05:11 -0400

    Tipos de dados primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Tipos de dados Primitivos
+
+// Numeros inteiros
+shot ano = 2024;
+int contador = 10;
+long populacaoMundial = 7800000000;
+
+// Numeros de ponto flutuante
+
+float temperatura = 43.6f;
+double precoProduto = 199.90;
+
+// Character 
+char inicial = "J";
+
+// Tipos logicos 
+bool = estaLogado = true;
+bool = ativo = false;
