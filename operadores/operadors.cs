ddbfbfff82371f2d60ae37b5aa2677dbbc5bee3a
João Paulo <joaopaulofam@gmail.com>
2024-11-01 17:19:06 -0400

    Continuacao Operadores

---
+++ operadores/operadors.cs
@@ -0,0 +1,8 @@
+double resultado1 = 10 + 8 + 12 / 3;
+Console.WriteLine(resultado);
+
+double resultado2 = (10 + 8 + 12) / 3;
+Console.WriteLine(resultado);
+
+double resultado3 = (2 + 8) * 2 / 4;
+Console.WriteLine(resultado);
